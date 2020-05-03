a886422ae4e15128743647f74d7f0275536fe918
Musab Agab <musabagab2010@gmail.com>
2020-05-04 02:48:44 +0300

    deleted old code

--- lib/core/services/api.dart
+++
@@ -1,4 +0,0 @@
-/// The service responsible for networking requests
-class Api {
-  static const endpoint = 'https://jsonplaceholder.typicode.com';
-}
--- lib/locator.dart
+++ lib/locator.dart
@@ -1,18 +1,13 @@
 import 'package:get_it/get_it.dart';
-import 'package:provider_architecture/core/services/authentication_service.dart';
 import 'package:provider_architecture/core/services/localstorage_service.dart';
 import 'package:provider_architecture/core/viewmodels/onboarding_model.dart';
-
-import 'core/services/api.dart';
 
 GetIt locator = GetIt();
 
 Future setupLocator() async {
   var instance = await LocalStorageService.getInstance();
-  locator.registerSingleton<LocalStorageService>(instance);
   // register services
-  locator.registerLazySingleton(() => Api());
-  locator.registerLazySingleton(() => AuthenticationService());
+  locator.registerSingleton<LocalStorageService>(instance);
   // register the viewmodel
   locator.registerFactory(() => OnboardingModel());
 }
