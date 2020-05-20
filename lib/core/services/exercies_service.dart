8f83fd174f2e38cad3ca167789ca285c3f86860d
Musab Agab <musabagab2010@gmail.com>
2020-05-21 01:39:09 +0300

    added constants for the duration

--- lib/core/services/exercies_service.dart
+++ lib/core/services/exercies_service.dart
@@ -1,12 +1,13 @@
 import 'package:provider_architecture/core/models/exercise.dart';
+import 'package:provider_architecture/core/shared/constants.dart';
 
 class ExericesService {
   final List<Exercise> fullBodyList = [
-    Exercise("PUSH-UPS", "30 s", "assetPath", "description"),
-    Exercise("PLANK", "30 s", "assetPath", "description"),
-    Exercise("SITUPS", "30 s", "assetPath", "description"),
-    Exercise("JUMPING JACKS", "30 s", "assetPath", "description"),
-    Exercise("BURPEES", "30 s", "assetPath", "description"),
+    Exercise("PUSH-UPS", EXERCISE_DURAION, "assetPath", "description"),
+    Exercise("PLANK", EXERCISE_DURAION, "assetPath", "description"),
+    Exercise("SITUPS", EXERCISE_DURAION, "assetPath", "description"),
+    Exercise("JUMPING JACKS", EXERCISE_DURAION, "assetPath", "description"),
+    Exercise("BURPEES", EXERCISE_DURAION, "assetPath", "description"),
   ];
 
   List<Exercise> getExercisesList(String selectedExerciseCategory) {
--- lib/core/shared/constants.dart
+++ lib/core/shared/constants.dart
@@ -1,3 +1,5 @@
 const String FULL_BODY = "Full body workout";
 const String LEG_WORKOUT = "Leg workout";
 const String ARM_WORKOUT = "Arm workout";
+
+const String EXERCISE_DURAION = "30 s";
