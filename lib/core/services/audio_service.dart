dfd1b3f6df159d6dc9eec66e559fdafb46baf209
Musab Nasredlin <musabagab2010@gmail.com>
2020-07-11 14:50:26 +0300

    added colors

--- lib/core/services/audio_service.dart
+++ lib/core/services/audio_service.dart
@@ -4,7 +4,7 @@ class AudioService {
   String startAudioPath = "audios/start.mp3";
   String doneAudioPath = "audios/done.mp3";
   String takeARestAudio = "audios/take_a_rest.mp3";
-
+  // added all the aduio here
   playStartAudio() async {
     AudioCache player = new AudioCache();
 
